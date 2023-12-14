3092956d3c06ee16a412291e17f6c9ea233ec642
allanDev9 <allanpenaguerra2003@gmail.com>
2023-12-14 09:27:07 -0600

    index cambios

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-const exports = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const students = [
+    {id: 1, name: 'Allan', age: 36, enroll: true},
+    {id: 2, nmae: 'Juan', age: 45, enroll: false},
+    {id: 3, nmae: 'Joel', age: 50, enroll: false},
+];
+
+app.get('/', (req, res) => {
+    res.send('Node JS api');
+});
+
+app.get('/api/students', (req, res) => {
+    res.send('students');
+});
+
+app.get('/api/students/:id', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if(!student) return res.status(404).send('Estudiante no encontrado');
+    else res.send(student);
+});
+
+app.post('/api/students', (req, res) => {
+    const student = {
+        id: students.length + 1,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        enroll: (req.body.enroll === 'true')
+    }
+
+    students.push(student);
+    res.send(student)
+
+});
+
+app.delete('/api/students', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if(!student) return res.status(404).send('Estudiante no encontrado');
+
+    const index = students.indexOf(student);
+    students.splice(index, 1);
+    res.send(student);
+});
+
+const port = process.env.port || 80
+app.listen(port, () => console.log('Escuchando en puerto ${port}...'));
+
+
+
